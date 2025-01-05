368681f2c3219e37cd82955777a3b3b73c3e61b4
Anil Singh <anil05.me@gmail.com>
2025-01-05 00:57:46 -0600

    Code for OpenAi  Integration

---
+++ src/main/java/com/example/springai/demo_spring_ai/GenerativeAiController.java
@@ -0,0 +1,18 @@
+package com.example.springai.demo_spring_ai;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GenerativeAiController {
+
+    @Autowired
+    ChatService chatService;
+
+    @GetMapping("ask-ai")
+    public String getResponse(@RequestParam String prompt){
+        return chatService.getResponse(prompt);
+    }
+}
